24fb4b019d9733d98e3c8832294199d6b6d2def6
Abanoub_Magdy <62855040+abanoubmagdy4@users.noreply.github.com>
2023-03-30 17:24:32 +0200

    Update EX4_insert_an_element_in_an_array.c

--- assignments/unit 2 _c_programming/4_unit2_lesson_4_C_Array_and_string/EX4_insert_an_element_in_an_array.c
+++ assignments/unit 2 _c_programming/4_unit2_lesson_4_C_Array_and_string/EX4_insert_an_element_in_an_array.c
@@ -3,33 +3,32 @@ Mastering Embedded System - Online Diploma
 Learn In Depth -- by eng keroles shenouda
         assignments - Home work 3
     4_unit2_lesson_4_C_Array_and_string
-    EX2_calculate_average_using_array
+    EX4_insert_an_element_in_an_array.c
+    
     author : Abanoub Magdy Nessiem
  */
 #include <stdio.h>
 int main() {
-	int i , n ,e,l ,Matrix[n] ;
-	fflush(stdin); fflush(stdout);
+	int i , n=5,e=25,l=5  ;
 	printf ("enter number of elements : ");
 	scanf("%d" , &n);
+	int Matttrix [n];
 	for(i = 0 ; i < n ; i++){
-		Matrix[i] = (i+1);
-			fflush(stdin); fflush(stdout);
-
-		printf ("%d\n", Matrix[i]);
-	}
+		Matttrix[i] = (i+1);
 		fflush(stdin); fflush(stdout);
+
+		printf ("%d\n", Matttrix[i]);
+	}
 
 	printf ("enter the element to be inserted ");
 	scanf("%d" , &e);
 	printf ("enter the Location");
 	scanf("%d" , &l);
-Matrix[l-1] = e ; 
+	Matttrix[l-1] = e ;
 	for(i = 0 ; i < n ; i++){
-	    	fflush(stdin); fflush(stdout);
 
-		printf ("%d", Matrix[i]);
+		printf ("%d  ", Matttrix[i]);
 	}
 
-return 0 ;
+	return 0 ;
 }
