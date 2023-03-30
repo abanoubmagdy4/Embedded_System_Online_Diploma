867db9a64dd745bcb733eb98a915eba637732b80
Abanoub_Magdy <62855040+abanoubmagdy4@users.noreply.github.com>
2023-03-31 00:21:32 +0200

    Update strings_EX1_find_the_frequency_of_characters_in_a_string.c

--- assignments/unit 2 _c_programming/4_unit2_lesson_4_C_Array_and_string/strings_EX1_find_the_frequency_of_characters_in_a_string.c
+++ assignments/unit 2 _c_programming/4_unit2_lesson_4_C_Array_and_string/strings_EX1_find_the_frequency_of_characters_in_a_string.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  int n = 20 ,i, sum =0;
+  char   text[n]  ;
+  char character ;
+  printf("enter a string");
+  gets(text);
+  printf("enter a charachter to find frequency");
+  scanf("%c", &character);
+  for (i = 0; i < n ; i++){
+      if (strcmp (text[i] , character) ==0){
+          sum += 1 ;
+          break ;
+      }
+  }
+  printf("%d", sum);
+    return 0;
+}
 
